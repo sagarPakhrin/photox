68c9b46c0e58290b7549019ceb1a2b2ce28960b4
Sagar Lama <sagarlama826@gmail.com>
2019-03-20 22:17:40 +0545

    pricing added

--- js/script.js
+++ js/script.js
@@ -13,4 +13,36 @@ $(document).ready(function(){
 				}
 		})
 
+		$(window).scroll(function(){
+				let position = $(this).scrollTop();
+				console.log(position);
+				if(position >= 500) {
+						$('.camera-img').addClass('fromLeft');
+						$('.mission-text').addClass('fromRight');
+				}
+		})
+		$('.gallery-list-item').click(function(){
+				let value = $(this).attr('data-filter');
+				if(value ==='all'){
+						$('.filter').show(300);
+				}
+				else {
+						$('.filter').not('.' + value).hide(300);
+						$('.filter').filter('.'+ value).show(300);
+				}
+		})
+
+		$('.gallery-list-item').click(function(){
+				$(this).addClass('active-item').siblings().removeClass('active-item');
+		});
+
+		$(window).scroll(function(){
+				let position = $(this).scrollTop();
+				if(position>=3976){
+						$('.card-1').addClass('moveFromLeft');
+						$('.card-3').addClass('moveFromRight');
+						$('.card-2').addClass('moveFromBottom');
+				}
+		})
+
 });
